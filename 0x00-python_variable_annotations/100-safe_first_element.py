cf40df4b44f90c9554c3f208895ac03d31d9837b
DevBasem <engr.basemahmed@gmail.com>
2024-06-06 12:20:38 -0700

    adv fix 1

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ Module to define a function with duck-typed annotations.
 
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Return the first element of a sequence safely.
@@ -13,7 +14,8 @@ def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
         lst (Sequence[Any]): The sequence to retrieve the first element from.
 
     Returns:
-        Union[Any, None]: The first element of the sequence, or None if the sequence is empty.
+        Union[Any, None]: The first element of the sequence,
+        or None if the sequence is empty.
     """
     if lst:
         return lst[0]
