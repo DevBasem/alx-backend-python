07c84a9cb3246841e6aaa658a905da9f1228f232
DevBasem <engr.basemahmed@gmail.com>
2024-06-06 11:52:47 -0700

    task 4 fix

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-"""
-This module defines and annotates several variables.
-"""
 
 
-a = 1  # type: int
-pi = 3.14  # type: float
-i_understand_annotations = True  # type: bool
-school = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
