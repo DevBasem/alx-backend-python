42666cb8affd1d98c72c3c099d9b3263b00183bc
DevBasem <engr.basemahmed@gmail.com>
2024-06-11 19:44:19 +0000

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async Comprehension
+"""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Coroutine that collects 10 random numbers asynchronously using an async comprehension.
+    """
+    return [i async for i in async_generator()]
