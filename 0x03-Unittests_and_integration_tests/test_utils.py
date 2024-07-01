1488074180519838a51f07b20e3b4bd6eba662f2
DevBasem <engr.basemahmed@gmail.com>
2024-07-01 06:47:30 -0700

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,28 +7,38 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """
-    Test cases for access_nested_map function.
+    Tests for the access_nested_map function.
     """
 
     @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Test access_nested_map function with valid inputs.
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_key):
         """
         Test access_nested_map function raises KeyError for non-existent keys.
-
-        Parameters:
-        - nested_map (dict): A nested dictionary.
-        - path (tuple): A sequence of keys.
-        - expected_key (str): The expected key that triggers the KeyError.
         """
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         
-        self.assertEqual(str(cm.exception), expected_key)
+        actual_key = str(cm.exception)  # Get the actual exception message
+        self.assertEqual(actual_key, expected_key)
+
 
 if __name__ == '__main__':
     unittest.main()
